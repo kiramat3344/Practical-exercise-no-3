2ef82889178776b4b6c8867a99b92e69426f2e25
kiramat3344 <kiramat0323@gmail.com>
2024-09-13 11:47:38 +0500

    its a exercise no 3

---
+++ script.js
@@ -0,0 +1,38 @@
+// chapter 15 Array method 
+// var ok = +prompt("Enter the number for cites");
+
+// var cites = ["lahore", "islamabad","peshawar","sindh","Balochistan","Mardan","Sewat"]
+// alert("Welcome to "+ cites[ok]);
+
+
+// Chapter 16 pop and push method
+// var ok = +prompt("Enter the number for cites");
+
+// var cites = ["lahore", "islamabad","peshawar","sindh","Balochistan","Mardan","Sewat"]
+
+// var t= cites.push("hello", "school");
+// console.log(t);
+// var cites = ["lahore", "islamabad","peshawar","sindh","Balochistan","Mardan","Sewat"]
+
+// var t= cites.pop();
+// console.log(t);
+
+
+// chapter 17 methods (shift, unshift, splice, slice)
+
+// let pets=["dog","cat","cow","elepent"];
+// i=pets.shift();
+// console.log(i);
+
+// let pets=["dog","cat","cow","elepent"];
+// i=pets.unshift("hello");
+// console.log(i);
+
+// let pets=["dog","cat","cow","elepent"];
+// i=pets.splice(1,2 ,"pig","duck");
+// console.log(i);
+
+// let pets=["dog","cat","cow","elepent"];
+// i=pets.slice(1,2 ,"pig","duck");
+// console.log(i);
+
